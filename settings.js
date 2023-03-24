49f528641e88a0ae74a9cf1c2ec946125eba7bba
Cipher <109347753+Yumethdeshan12345@users.noreply.github.com>
2023-03-24 18:09:03 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -27,7 +27,7 @@ global.socialm = "GitHub: Dark" //ur github or insta name
 global.location = "USA, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.session = " " //session Id
+global.session = "UGsxNVNDaVI=" //session Id
 global.ownernomer = "94764186258" //ur number
 global.lang = "EN" //Select Language "EN" to English "SI" to sinhala 
 global.premium = ['94764186258'] //ur premium number
